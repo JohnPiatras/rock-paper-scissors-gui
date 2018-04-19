9f718b86d109049f062f8b085f3fa98bec5a1f77
burns <john.piatras@gmail.com>
2018-04-19 14:29:31 +0100

    Game logic is done, added result dialog

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 "use strict";
 //Rock, paper, scissors
-//Browser console Rock, Paper, Scissors game
 //John Burns
 
 //Use a look up table to pick a winner
@@ -9,90 +8,168 @@
 //  Paper     win     draw     lose
 //  Scissors  lose    win      draw
 const rules = {rock: {rock: "draw", paper: "lose", scissors: "win"},
-        Paper: {rock: "win", paper: "draw", scissors: "lose"},
-        Scissors: {rock: "lose", paper: "win", scissors: "draw"}};
+        paper: {rock: "win", paper: "draw", scissors: "lose"},
+        scissors: {rock: "lose", paper: "win", scissors: "draw"}};
 const valid_choices = ["rock", "paper", "scissors"];
 
-initialise();
-let game_state = "idle";
+
+
+//declare some globals
+let game_state = "";
 let player_choice = "";
 let computer_choice = "";
+let music_on = true;
+let player_sprite;
+let computer_sprite;
+let player_score;
+let computer_score;
+let rounds_played; //use to indicate no games have been played yet
+
+initialise();
 
 function initialise(){
+    player_sprite = document.getElementById("player-sprite");
+    computer_sprite = document.getElementById("computer-sprite");
+
     let bttns = document.querySelectorAll(".button");
     bttns.forEach((bttn) => bttn.addEventListener('click', onClickButton));
 
     let music_loop = document.getElementById("music-loop");
-    music_loop.loop = true;
-    music_loop.play();
-    //music_loop.addEventListener("ended", onEndedMusicLoop);
     music_loop.addEventListener("ended", gameStateChange);
+
     let music_finish = document.getElementById("music-finish");
     music_finish.loop = false;
+    music_finish.addEventListener("ended", gameStateChange);
 
-    document.getElementById("music-finish").addEventListener("ended", gameStateChange);
-    document.getElementById("snd-choose").addEventListener("ended", gameStateChange);
+    document.getElementById("player-sprite").addEventListener("animationend", gameStateChange);
 
+    rounds_played = -1;
+    showDialogBox("Rock, paper scissors. Best of 5 rounds", "Play");
 }
 
+function clearClassList(element){
+    let classList = element.classList;
+    while(classList.length > 0){
+        classList.remove(classList.item(0));
+    }
+}
+
+function resetGame(){
+    player_score = 0;
+    computer_score = 0;
+    rounds_played = 0;
+    updateScore();
+    resetRound();
+}
+
+function resetRound(){
+    game_state = "idle";
+    player_choice = "";
+    computer_choice = "";
+
+    clearClassList(player_sprite);
+    clearClassList(computer_sprite);
+    player_sprite.classList.add("player-sprite-idle");
+    computer_sprite.classList.add("computer-sprite-idle");
+
+    document.getElementById("dialog-box").classList.add("hide");
+    document.getElementById("hand-buttons").classList.remove("hide");
+
+    let music_loop = document.getElementById("music-loop");
+    music_loop.loop = true;
+    if(music_on)music_loop.play();
+
+    updateScore();
+}
+
+function updateScore(){
+    let score = document.querySelector("#score h3");
+    score.textContent = player_score + " : " + computer_score;
+
+    document.querySelector("#score h2").textContent = "Round " + (rounds_played + 1);
+}
+
+function showDialogBox(text, bttn_text){
+    let box = document.getElementById("dialog-box");
+    box.children[0].textContent = text;
+    box.children[1].textContent = bttn_text;
+    box.classList.remove("hide");
+}
 
 //Event handlers
+
+
 function onClickButton(e){
     let id = e.target.getAttribute('id');
-    document.getElementById("music-loop").loop = false;
-    document.getElementById("hand-buttons").classList.add("hide");
     switch(id){
         case "rock-bttn":
             console.log("rock button clicked");
             player_choice = "rock";
+            document.getElementById("hand-buttons").classList.add("hide");
+            gameStateChange();
             break;
         case "scissors-bttn":
             console.log("Scissors button clicked");
             player_choice = "scissors";
+            document.getElementById("hand-buttons").classList.add("hide");
+            gameStateChange();
             break;
         case "paper-bttn":
             console.log("Paper button clicked");
             player_choice = "paper";
+            document.getElementById("hand-buttons").classList.add("hide");
+            gameStateChange();
+            break;
+        case "continue-bttn":
+            if(rounds_played >= 0 && rounds_played < 5)
+                resetRound();
+            else   
+                resetGame();
+            break;
+        case "music-bttn":
+            if(music_on){
+                music_on = false;
+                document.getElementById("music-loop").pause();
+            }else{
+                music_on = true;
+                document.getElementById("music-loop").play();
+            }
+            
+            
+            document.getElementById("music-bttn").classList.toggle("music-bttn-off");
             break;
     }
+    
 }
 
-function onEndedMusicLoop(e){
-    console.log("onFinishMusicLoop() fired");
-    document.getElementById("music-finish").play();
-    doPlayersChoose();
-}
-
-function doPlayersChoose(){
-    let snd = document.getElementById("snd_choose");
-}
-
-function onEndedPlayersChoose(){
-
-}
 
 function gameStateChange(e){
-    let player_sprite = document.getElementById("player-sprite");
-    let computer_sprite = document.getElementById("computer-sprite");
     switch(game_state){
         case "idle":
             console.log("State: " + game_state);
-            game_state = "aboutToChoose";
-            console.log("    New state: " + game_state);
             document.getElementById("music-loop").loop = false;
-        case "aboutToChoose":
-            console.log("State: " + game_state);   //switch to choosing
-            game_state = "choosing";
+
+            if(music_on){
+                game_state = "roundStart";
+            }else{
+                game_state = "roundPlay";
+                //we don't have the ended event of music-loop to fire this again so do it here
+                gameStateChange();
+            }
+            console.log("    New state: " + game_state);
+            break;
+
+        case "roundStart":
+            console.log("State: " + game_state);   
+            game_state = "roundPlay";
             console.log("    New state: " + game_state);
             document.getElementById("music-finish").play();
             break;
-        case "choosing":
-            console.log("State: " + game_state);   //switch to choosing
-            game_state = "roundEnded";
-            console.log("    New state: " + game_state);
-            document.getElementById("snd-choose").play();
-            console.log("    start choosing animations here");
 
+        case "roundPlay":
+            console.log("State: " + game_state);   
+            
+            document.getElementById("snd-choose").play();
             player_sprite.classList.remove("player-sprite-idle");
             player_sprite.classList.add("player-sprite-choosing");
             computer_sprite.classList.remove("computer-sprite-idle");
@@ -100,52 +177,64 @@ function gameStateChange(e){
 
             //make the computers choice
             computer_choice = computerPlay();
-            console.log("Switching to state roundEnded");
+            game_state = "roundResult";
+            console.log("    New state: " + game_state);
             break;
-        case "roundEnded":
+        case "roundResult":
             console.log("State: " + game_state);   //switch to choosing
             player_sprite.classList.remove("player-sprite-choosing");
             player_sprite.classList.add("player-sprite-" + player_choice);
             computer_sprite.classList.remove("computer-sprite-choosing");
             computer_sprite.classList.add("computer-sprite-" + computer_choice);
-            console.log("Reset round");
+
+            let outcome = rules[player_choice][computer_choice];
+            if(outcome === "win"){
+                player_score++;
+                
+            }else if(outcome === "lose"){                
+                computer_score++;
+            }
+
+            updateScore();
+            rounds_played++;
+            if(rounds_played < 5){
+                showOutcomeDialog(outcome);
+            }else{  
+                showGameOverDialog();
+            }
             break;
     }
 }
 
+function showOutcomeDialog(outcome){
+    switch(outcome){
+        case "win":
+            showDialogBox("Huh, you won this round...", "Continue");
+            break;
+        case "lose":
+            showDialogBox("Haha, I win this round!", "Continue");
+            break;
+        case "draw":
+            showDialogBox("It's a draw!", "Continue");
+            break;
+    }
+}
 
+function showGameOverDialog(){
+    let text;
+    if(player_score > computer_score){
+        text = "Game Over! You won " + player_score + " to " + computer_score;
+    }else if(computer_score > player_score){
+        text = "Game Over! I won " + computer_score + " to " + player_score;
+    }else{
+        text = "Game Over! It's a draw!";
+    }
+
+    showDialogBox(text, "Play Again");
+}
 
 
 function computerPlay() {
     let n = Math.floor(Math.random() * 3);
     return valid_choices[n];
 }
-
-
-//Play a game of 5 rounds and show the result.
-function game() {
-    let playerWinCount = 0;
-    let computerWinCount = 0;
-    for ( let round = 0; round < 5; round++ ) {
-        let playerSelection = null;
-        let computerSelection = computerPlay();
-
-        while(!playerSelection){
-            playerSelection = validatePlayerInput(window.prompt("Enter rock, paper or scissors:"));
-        }
-
-        let result = rules[playerSelection][computerSelection];
-
-        if(result === "win") {
-            playerWinCount++;
-        } else if (result === "lose") {
-            computerWinCount++;
-        }
-
-        console.log("Round " + (round + 1));
-        console.log("    " + buildResultString(result, playerSelection, computerSelection));
-    }
-    showWinner(playerWinCount, computerWinCount);
-}
-
-
